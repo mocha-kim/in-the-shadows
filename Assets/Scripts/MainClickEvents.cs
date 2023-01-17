426e6d9a22d60150fd89bb00dce4687d293c9f03
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2023-01-17 18:13:02 +0900

    feat: click event call methods

---
+++ Assets/Scripts/MainClickEvents.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InTheShadow.Events
+{
+    public class MainClickEvents : MonoBehaviour
+    {
+        public void OnClickStart()
+        {
+
+        }
+
+        public void OnClickExit()
+        {
+
+        }
+
+        public void OnClickNormalMode()
+        {
+
+        }
+
+        public void OnClickTestMode()
+        {
+
+        }
+    }
+}
